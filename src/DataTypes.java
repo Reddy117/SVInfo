90724ab87141d9f278a555cf2852d1a8e71ee27e
pc <pc@192.168.56.1>
2020-11-13 09:31:56 +0530

    I am commiting day1

---
+++ src/DataTypes.java
@@ -0,0 +1,15 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int i=10; //int is a data type represents numeric values
+		long l=546544565; //long is also a data type represents long range of numbers more than 10 digits
+		float f=12.30f; // float is data type used to represent floating numbers float accepts 7 digits after point
+		double d=50.50; //double is the data type represent decimals after point 13 digits
+		boolean b=true; //boolean represent either true or false
+		char c='C'; // char accepts single character in single quotes
+		
+		String s="Welcome to java";
+		System.out.println(i);
+	}
+}
